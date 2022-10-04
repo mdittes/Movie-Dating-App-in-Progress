44490a424bbad465b9430643d08ff685ef5480a0
Sucram <marcuscrabtree4@gmail.com>
2022-10-04 15:59:14 -0400

    updated styling

--- client/src/components/Login.js
+++ client/src/components/Login.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React, {useState} from 'react'
 
 function Login() {
-  return (
-    <div>
-        <h1>Login</h1>
-    </div>
-  )
+    let [authMode, setAuthMode] = useState("signin")
+  
+    const changeAuthMode = () => {
+      setAuthMode(authMode === "signin" ? "signup" : "signin")
+    }
+  
+    if (authMode === "signin") {
+      return (
+        <div className="Auth-form-container">
+          <form className="Auth-form">
+            <div className="Auth-form-content">
+              <h3 className="Auth-form-title">Sign In</h3>
+              <div className="form-group mt-3">
+                <label>Email address</label>
+                <input
+                  type="email"
+                  className="form-control mt-1"
+                  placeholder="Name"
+                />
+              </div>
+              <div className="form-group mt-3">
+                <label>Password</label>
+                <input
+                  type="password"
+                  className="form-control mt-1"
+                  placeholder="Enter password"
+                />
+              </div>
+              <div className="d-grid gap-2 mt-3">
+                <button style={{backgroundColor: "#529F8C", color: "#F6C6BF" }} type="submit">
+                  Submit
+                </button>
+              </div>
+              <p className="text-center mt-2">
+                Forgot <a href="#">password?</a>
+              </p>
+            </div>
+          </form>
+        </div>
+      )
+    }
 }
 
 export default Login
