6279c2fb8d22b35857a6565cfcad3b9e7a529326
Matt Dittes <mattdittes@gmail.com>
2022-10-04 15:24:43 -0400

    Updated routes and controllers

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,9 @@
 class UsersController < ApplicationController
     before_action :authorize
 
-    def index 
-        render json: User.all
-    end
+    # def index 
+    #     render json: User.all
+    # end
 
     def show 
         user = User.find_by(id: session[:user_id])
